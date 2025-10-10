2ec25584d0288a0bf8cedca01cd9652325b10e94
Abshar777 <absharameen625@gmail.com>
2025-10-10 14:04:11 +0530

    feat: done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
           <Indexlayout>{children}</Indexlayout>
         </main>
         <Scripts />
-      </body>Screenshot 2025-10-06 141113.png
+      </body>
     </html>
   );
 }
